224a451a6fe2a1f217617d01d9df16289b139fa0
Jun Simons <junsimons@dcc-wl-738.dynamic2.rpi.edu>
2025-04-30 14:58:19 -0400

    fix promise typing

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -11,8 +11,15 @@ export async function generateStaticParams() {
   return getAllPostMeta().map(({ slug }) => ({ slug }))
 }
 
-export default async function Post({ params }: { params: { slug: string } }) {
-  const { data, content } = getPost(params.slug)
+export default async function Post({
+  params,
+  }: {
+  // params isPromise of params 
+  params: Promise<{ slug: string }>
+  }) {
+  // await the Promise to get the real slug
+  const { slug } = await params
+  const { data, content } = getPost(slug)
   return (
     <main className="max-w-3xl mx-auto px-16 py-16">
       <h1 className="text-4xl font-monoreg font-semibold mb-4">{data.title}</h1>

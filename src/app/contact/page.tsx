aa6de3203bc11372194575c723ab599127254560
Jun Simons <jdsimons017@gmail.com>
2025-06-12 22:59:25 -0400

    update email to svg

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,10 +7,30 @@ export default function Blog() {
     <main className="max-w-3xl mx-auto px-4 py-16">
     <Navbar visible={true} />
       <br></br>
-      <h1 className="text-4xl  font-monoreg font-semibold mb-8">Contact me &#x263A;</h1>
-      <div className="text-2xl font-monoreg"> 
+      <h1 className="text-2xl  font-monoreg font-semibold mb-8">Contact me &#x263A;</h1>
+      <div className="text-xl font-monoreg"> 
         <p>
-          Email me at jdsimons017@gmail.com,<br></br><br></br>
+          Email me at<br></br>
+
+          <a href="mailto:jdsimons017@gmail.com">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="260" height="30"
+            viewBox="0 0 260 24"
+            className="inline-block align-middle"
+          >
+            <text
+              x="0" y="18"
+              fontFamily="InputMonoRegular, monospace"
+              fontSize="16"
+              fill="#171717"
+            >
+              jdsimons017@gmail.com
+            </text>
+          </svg>
+        </a>
+          
+          <br></br><br></br>
           or find me on&nbsp;
           <a 
             href="https://www.linkedin.com/in/jun-simons/"
